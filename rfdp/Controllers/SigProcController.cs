cd77cb5b808f58e2b832c641d94e89c4a749f824
jeffrey <jeffrl@live.com>
2013-10-24 20:39:50 -0700

    added basic ajax func

---
+++ rfdp/Controllers/SigProcController.cs
@@ -0,0 +1,34 @@
+﻿using rfdp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rfdp.Controllers
+{
+    public class SigProcController : Controller
+    {
+        //
+        // GET: /SigProc/
+
+        public ActionResult Index(string DataSetLists = null, double fs = 1000)
+        {
+            List<SelectListItem> DataSetList = new List<SelectListItem>();
+            DataSetList.Add(new SelectListItem { Text = "data0.txt", Value = "data0.txt" });
+            DataSetList.Add(new SelectListItem { Text = "data1.txt", Value = "data1.txt" });
+            DataSetList.Add(new SelectListItem { Text = "data2.txt", Value = "data2.txt" });
+            DataSetList.Add(new SelectListItem { Text = "data3.txt", Value = "data3.txt" });
+
+            ViewBag.DataSetLists = DataSetList;
+
+            SigProc sig = new SigProc();
+            sig.dataset = DataSetLists;
+            sig.fs = fs;
+            sig.mean = 23;
+            sig.rms = 110;
+            return View(sig);
+        }
+
+    }
+}
---
+++ rfdp/Models/SigProc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rfdp.Models
+{
+    public class SigProc
+    {
+        public string dataset { get; set; }
+        public double fs { get; set; }
+        public double mean { get; set; }
+        public double rms { get; set; }
+    }
+}
